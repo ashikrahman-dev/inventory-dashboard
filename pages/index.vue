5af4af1ec24ff8b911301ad03c022bacab43aa86
Ashik <ashikur1993@gmail.com>
2022-12-22 17:27:01 +0600

    Vue Project

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav>
+            <ul class="grid grid-cols-6 gap-2 bg-amber-100 text-amber-800 p-4">
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+                <li><NuxtLink to="/login">Login</NuxtLink></li>
+            </ul>
+        </nav>
+        <h1>Home page</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio porro quos perspiciatis. Autem assumenda pariatur necessitatibus ipsa harum quisquam! Reiciendis nulla neque iste animi, eius fugiat incidunt necessitatibus ut excepturi?</p>
+    </div>
+</template>
+
+<script setup>
+
+   
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/registration/index.vue
@@ -0,0 +1,76 @@
+
+<template>
+    <div class="login-bg h-screen grid items-center">
+        <div class="xl:container mx-auto ">
+            <div class="py-12 px-4 sm:px-6 lg:px-8 ">
+                <div class="w-full max-w-md space-y-8 login-wrap bg-white" >
+                    <div>
+                        <img class="h-6 w-auto" src="../../assets/img/logo.svg" alt="Your Company" />
+                        <h2 class="mt-6 text-4xl font-bold tracking-tight text-gray-900">Registration</h2>
+                    </div>
+                    <form class="mt-8 space-y-6" action="#" method="POST">
+                        <input type="hidden" name="remember" value="true" />
+                        <div class="-space-y-px rounded-md shadow-sm">
+                            <div>
+                                <label for="email-address" class=" text-gray-900">Email</label>
+                                <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+                            </div>
+                            <div>
+                                <label for="password" class="text-gray-900">Password</label>
+                                <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+                            </div>
+                        </div>
+            
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                                <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+                            </div>
+                
+                            <div class="text-sm">
+                                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+                            </div>
+                        </div>
+                
+                        <div class="grid gap-4">
+                            <NuxtLink to="/login">
+                                <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                        <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                    </span>
+                                    Sign in
+                                </button>
+                            </NuxtLink>
+                            
+                            <NuxtLink to="/registration">
+                                <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                        <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                    </span>
+                                    registration
+                                </button>
+                            </NuxtLink>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script setup>
+//   import { LockClosedIcon } from '@heroicons/vue/20/solid'
+  </script>
+
+<style scoped>
+    .login-bg {
+        background: url('../../assets/img/login-bg.jpg');
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
+    }
+    .login-wrap {
+        border-radius: 20px;
+        padding: 50px;
+    }
+</style>
