07b534676568638a5ed67b2a206416e63a9df15d
Ashik <ashikur1993@gmail.com>
2022-12-29 18:55:57 +0600

    hudai vungvang

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
-    "./src/components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
